a3316ea9ddc709743d23813b6a3a0e06d4ea5f21
Sergey Tursanov <gsr@npc-spektr.ru>
2024-03-22 20:27:04 +0400

    Убрана отладочная печать из kkt/kkt.c

--- kkt/xml.c
+++ kkt/xml.c
@@ -10,15 +10,11 @@
 static inline int call_xml_cbk(kkt_xml_callback_t cbk, bool check, int evt,
 	const char *name, const char *val)
 {
-	printf("%s: check = %d; evt = %d; name = %s; val = %s\n", __func__,
-		check, evt, name ? name : "NULL", val ? val : "NULL");
 	return (cbk == NULL) ? E_OK : cbk(check, evt, name, val);
 }
 
 static bool handle_xml_node(xmlNode *first, bool check, kkt_xml_callback_t cbk, int *ecode)
 {
-	printf("%s (%p): first = %p; check = %d; cbk = %p; ecode = %p\n", __func__,
-		handle_xml_node, first, check, cbk, ecode);
 	bool ret = true;
 	*ecode = E_OK;
 	for (xmlNode *node = first; node != NULL; node = node->next){
@@ -60,8 +56,6 @@ static bool handle_xml_node(xmlNode *first, bool check, kkt_xml_callback_t cbk, 
 
 static bool handle_xml(xmlDoc *xml, bool check, kkt_xml_callback_t cbk, int *ecode)
 {
-	printf("%s (%p): xml = %p; check = %d; cbk = %p; ecode = %p\n", __func__,
-		handle_xml, xml, check, cbk, ecode);
 	*ecode = E_OK;
 	int rc = call_xml_cbk(cbk, check, KKT_XML_BEGIN, NULL, NULL);
 	if (check && (rc != E_OK)){
