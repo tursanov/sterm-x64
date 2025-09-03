211f4e94b21eeb52c99ae3bf8132032395c2e380
Alex Popov <alex.p.popov@gmail.com>
2025-09-03 07:12:43 +0400

    Изменил работу с битовыми полями, исправил ошибку с чеком коррекции

--- gui/newcheque.c
+++ gui/newcheque.c
@@ -1030,7 +1030,7 @@ bool newcheque_print(window_t *w) {
 	}
 
 	if (agent != NULL) {
-/*		ffd_tlv_add_uint8(1057, 1 << agent->pay_agent);
+		ffd_tlv_add_uint8(1057, 1 << agent->pay_agent);
 		switch (agent->pay_agent) {
 			case 0:
 			case 1:
@@ -1053,7 +1053,7 @@ bool newcheque_print(window_t *w) {
 			case 6:
 				ffd_tlv_add_string(1171, agent->supplier_phone);
 				break;
-		}*/
+		}
 	} else if (newcheque.agent_data_size > 0) {
 		for (uint8_t *p = newcheque.agent_data,
 				*end = p + newcheque.agent_data_size; p < end; ) {
