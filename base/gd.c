1f3279a8a47b5730146cc0d90e265c2d3f881d76
Sergey Tursanov <gsr@npc-spektr.ru>
2025-09-29 14:37:22 +0400

    Бит идентификатора "Печать на ККТ шаблонами" формируется без учёта признака фискального режима в настройках.

--- base/gd.c
+++ base/gd.c
@@ -164,8 +164,9 @@ static uint8_t get_tcap_byte(void)
 /* 77 -- 7f */	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	/* . . . . . . . . */
 	};
 	uint8_t ret = TCAP_TERM;
-	if (cfg.has_kkt && (kkt != NULL) && cfg.fiscal_mode){
-		ret |= TCAP_KKT;
+	if (cfg.has_kkt && (kkt != NULL)){
+		if (cfg.fiscal_mode)
+			ret |= TCAP_KKT;
 		if (kkt_has_param("SUPPORT_ESC_R"))
 			ret |= TCAP_XSLT;
 	}
