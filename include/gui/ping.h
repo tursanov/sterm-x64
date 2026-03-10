1f01f4178172ddeeb44265bca32ed6408334e7fd
Sergey Tursanov <tursanov@npc-spektr.ru>
2026-03-10 10:27:04 +0300

    Из ветки old-pos перенесены изменения в ping.

--- include/gui/ping.h
+++ include/gui/ping.h
@@ -1,6 +1,10 @@
 /*
- * Проверка линии TCP/IP посылкой icmp-пакетов (ping).
- * (c) gsr, Alex P. Popov. 2002,2004.
+ * Проверка связи по TCP/IP с помощью ICMP и подключения по TCP.
+ * 11.05.2019 добавлена проверка ОФД путём установки соединения TCP.
+ * 08.11.2024 процессинговые серверы проверяются так же, как и ОФД.
+ * 06.03.2026 исправлена ошибка формирования ICMP-пакетов, изменён состав данных.
+ * 09.03.2026 при анализе ответов ICMP используется буфер данных.
+ * (c) gsr, Alex P. Popov 2002, 2004, 2005, 2019, 2024, 2026.
  */
 
 #if !defined GUI_PING_H
@@ -10,19 +14,31 @@
 extern "C" {
 #endif
 
+#include "gd.h"
 #include "kbd.h"
 #include "gui/scr.h"
 
 #define ICMP_PROTO_NAME		"icmp"
-#define RCV_BUF_LEN		49152		/* 48K */
-#define PING_DATA_LEN		32
 #define MAX_IP_LEN		60
 #define MAX_ICMP_LEN		76
-#define ICMP_DATA		"__sterm__"
 
-/* Все интерваля задаются в сотых долях секунды */
-#define ICMP_PING_INTERVAL		10	/* 0.1 с */
-#define ICMP_WAIT_INTERVAL		300	/* 3 с */
+/* Данные, передаваемые в ICMP-пакете */
+struct ping_data {
+	uint32_t sig;				/* слово PING */
+#define STERM_PING_SIG	0x474e4950
+	uint32_t uptime;			/* время в секундах с момента включения теминала */
+	uint32_t t;				/* время отправки пакета */
+	uint8_t version[3];			/* версия терминала */
+	uint16_t crc;				/* контрольная сумма терминала */
+	uint8_t nr[TERM_NUMBER_LEN];		/* заводской номер терминала */
+	uint8_t gaddr;				/* групповой адрес терминала */
+	uint8_t iaddr;				/* индивидуальный адрес терминала */
+} __attribute__((__packed__));			/* должно быть 32 байта */
+
+#define PING_DATA_LEN	sizeof(struct ping_data)
+
+/* Все интервалы задаются в сотых долях секунды */
+#define ICMP_PING_INTERVAL		100	/* 1 сек */
 #define NR_PINGS			3
 
 extern bool	init_ping();
