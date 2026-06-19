7dc5cf93faac917f3bb3cb3628642ff921ac8224
Sergey Tursanov <gsr@npc-spektr.ru>
2026-06-19 19:10:46 +0400

    Обмен с ККТ записывается в файл журнала, как это было изначально.

--- kkt/kkt.c
+++ kkt/kkt.c
@@ -373,11 +373,10 @@ static bool do_transaction(uint8_t prefix, uint8_t cmd, void *param)
 	uint32_t timeout = get_timeout(prefix, cmd);
 	struct timeval t0;
 	gettimeofday(&t0, NULL);
-	//printf("%s: parser = %p; timeout = %u; kkt_tx_len = %zu\n",
-	//	__func__, parser, timeout, kkt_tx_len);
 	if (kkt_tx_len > 0){
 		ssize_t rc = kkt_io_write(&timeout);
-		//printf("%s: rc = %zd\n", __func__, rc);
+		if (rc > 0)
+			log_data("kkt", "’Œ --> ŠŠ’", kkt_tx, rc);
 		if (rc != kkt_tx_len)
 			ret = kkt_on_com_error(timeout);
 	}
@@ -407,6 +406,7 @@ static bool do_transaction(uint8_t prefix, uint8_t cmd, void *param)
 			((cmd == KKT_SRV_BEGIN_DOC) || (cmd == KKT_SRV_SEND_DOC) ||
 			 (cmd == KKT_SRV_END_DOC)))
 		flags = KLOG_REC_APC;
+	log_data("kkt", "ŠŠ’ --> ’Œ", kkt_rx, kkt_rx_len);
 	klog_write_rec(hklog, &t0, kkt_tx, kkt_tx_len, kkt_status, kkt_rx, kkt_rx_len, flags);
 	return ret;
 }
